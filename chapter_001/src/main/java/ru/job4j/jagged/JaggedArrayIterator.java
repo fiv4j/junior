1ce459d634ffe7e20b168a0f24f9068a3af68972
fiv4j <ifrolov@rambler.ru>
2019-12-05 19:26:05 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#208493]

---
+++ chapter_001/src/main/java/ru/job4j/jagged/JaggedArrayIterator.java
@@ -0,0 +1,45 @@
+package ru.job4j.jagged;
+
+import java.util.Iterator;
+
+public class JaggedArrayIterator implements Iterator<Integer> {
+
+    private final int[][] values;
+    private int row = 0;
+    private int cell = 0;
+    private int idx = 0;
+
+    public JaggedArrayIterator(final int[][] values) {
+        this.values = values;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return idx < this.sumLength();
+    }
+
+    @Override
+    public Integer next() {
+        int result = values[this.row][this.cell];
+        increaseIdx();
+        return result;
+    }
+
+    private int sumLength() {
+        int result = 0;
+        for (int[] inner : this.values) {
+            result += inner.length;
+        }
+        return result;
+    }
+
+    private void increaseIdx() {
+        this.idx++;
+        if (this.cell + 1 < values[this.row].length) {
+            this.cell++;
+        } else {
+            this.row++;
+            this.cell = 0;
+        }
+    }
+}
