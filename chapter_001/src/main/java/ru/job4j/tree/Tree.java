4b0d9c8eebf7209c0d15c46c87d39f5a0214d1de
fiv4j <ifrolov@rambler.ru>
2019-12-23 11:29:12 +0300

    1. Создать элементарную структуру дерева [#208513]

---
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,83 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    private Node<E> root;
+    private int modCount = 0;
+
+
+    public Tree(E rootElem) {
+        this.root = new Node<>(rootElem);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> parentNode = findBy(parent);
+        if (parentNode.isEmpty()) {
+            return false;
+        }
+        for (Node<E> currentChild : parentNode.get().leaves()) {
+            if (currentChild.eqValue(child)) {
+                return false;
+            }
+        }
+        parentNode.get().add(new Node<>(child));
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+
+            private final int expectedModCount = modCount;
+            Queue<Node<E>> elements = new LinkedList<>(Arrays.asList(root));
+
+            private void addToQueue(Node<E> item) {
+                for (Node<E> child : item.leaves()) {
+                    elements.offer(child);
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return elements.peek() != null;
+            }
+
+            @Override
+            public E next() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                addToQueue(elements.peek());
+                return elements.poll().getValue();
+            }
+        };
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,115 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class TreeTest {
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(
+                tree.findBy(6).isPresent(),
+                is(true)
+        );
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionalEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        assertThat(
+                tree.findBy(7).isPresent(),
+                is(false)
+        );
+    }
+
+    @Test
+    public void whenAddedElementAlreadyExistThenReturnFalse() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+
+        boolean result = tree.add(1, 2);
+        boolean expected = false;
+        assertThat(result, is(expected));
+
+    }
+
+    @Test
+    public void whenIteratingBy6ElementsThenHasNextAndNextReturnCorrectValues() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        Iterator<Integer> iterator = tree.iterator();
+
+        for (int i = 1; i <= 6; i++) {
+            assertThat(iterator.hasNext(), is(true));
+            assertThat(iterator.next(), is(i));
+        }
+    }
+
+    @Test
+    public void whenIteratingThenAfterLastElementHasNextReturnFalse() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        Iterator<Integer> iterator = tree.iterator();
+        for (int i = 1; i <= 6; i++) {
+            iterator.next();
+        }
+
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenIteratingThenAfterLastElementNextThrowException() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        Iterator<Integer> iterator = tree.iterator();
+        for (int i = 1; i <= 6; i++) {
+            iterator.next();
+        }
+
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIteratingAndAddNewElementThenNextThrowException() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        Iterator<Integer> iterator = tree.iterator();
+        iterator.next();
+
+        tree.add(5, 7);
+
+        iterator.next();
+    }
+}
