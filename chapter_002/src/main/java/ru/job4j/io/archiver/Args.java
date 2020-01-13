d8bfb82b45c9006698246c2db19b54fa5bc67dad
fiv4j <ifrolov@rambler.ru>
2020-01-13 13:05:13 +0300

    4. Архивировать проект. [#208522]. Initial commit.

---
+++ chapter_002/src/main/java/ru/job4j/io/archiver/Args.java
@@ -0,0 +1,18 @@
+package ru.job4j.io.archiver;
+
+import java.io.File;
+
+public class Args {
+
+    public String directory() {
+        return null;
+    }
+
+    public String exclude() {
+        return null;
+    }
+
+    public File output() {
+        return null;
+    }
+}
