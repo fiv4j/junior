be7c19a888887033d922a1ebcbe1a36d5e57d435
fiv4j <ifrolov@rambler.ru>
2020-08-07 18:51:33 +0300

    Исправить код класс SimpleGenerator. [#208554]

---
+++ chapter_004/src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+    @Ignore
+    @Test
+    public void whenTemplateWorkWell() {
+        Generator test = new SimpleGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+
+        String result = test.produce(template, map);
+        String expected = "I am a Petr Arsentev, Who are you?";
+
+        assertThat(result, is(expected));
+    }
+
+    @Ignore
+    @Test(expected = NoSuchElementException.class)
+    public void whenMapNotContainsTemplateKey() {
+        Generator test = new SimpleGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+
+        String result = test.produce(template, map);
+    }
+
+    @Ignore
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenMapContainsExcessKey() {
+        Generator test = new SimpleGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        map.put("object", "plane");
+
+        String result = test.produce(template, map);
+    }
+
+}
